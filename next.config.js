f304b619aec2b9b3817e4eb28e9a1f55977a14d0
abdullahbilal43 <ab784593@gmail.com>
2025-04-17 18:18:11 +0500

    working on contact form

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     serverActions: true, // Optional, only if you use it
   },
