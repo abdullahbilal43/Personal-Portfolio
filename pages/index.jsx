b9b6028f130b9781f77ca434451b7673c453ea31
abdullahbilal43 <ab784593@gmail.com>
2025-04-17 14:42:30 +0500

    working on contact form

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary/60 pt-1 overflow-y-auto w-full h-screen overflow-hidden">
+    <div className="bg-primary/60 pt-1 h-full  min-h-screen overflow-y-auto w-full ">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
